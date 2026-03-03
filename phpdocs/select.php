5bb8e5be5012d3de33b035c9e194d12c16eaf296
brycecodotco <gxcreeper@gmail.com>
2026-03-03 14:56:48 +0800

    added

--- phpdocs/select.php
+++ phpdocs/select.php
@@ -1,6 +1,7 @@
 <?php
 require 'config.php';
 
-$stmt = $pdo->query("SELECT * FROM users");
+$stmt = $pdo->query("SELECT users.users_id, users.name, users.email, orders.product, orders.amount  FROM users
+                     INNER JOIN orders ON users.users_id = orders.user_id;");
 $users = $stmt->fetchall(PDO::FETCH_ASSOC);
 ?>
